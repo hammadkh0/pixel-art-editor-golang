4eb4b161bd84877a36c176e0380a20beed59d11d
hammadkh0 <hammad5718@gmail.com>
2024-07-14 13:54:38 +0500

    feat: added canvas to the main application

--- pixl/pixl.go
+++ pixl/pixl.go
@@ -3,9 +3,11 @@ package main
 import (
 	"image/color"
 	"pixel-art-editor-golang/apptype"
+	"pixel-art-editor-golang/pxcanvas"
 	"pixel-art-editor-golang/swatch"
 	"pixel-art-editor-golang/ui"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
@@ -19,7 +21,17 @@ func main() {
 		SwatchSelected: 0,
 	}
 
+	pixlCanvasConfig := apptype.PxCanvasConfig{
+		DrawingArea:  fyne.NewSize(600, 600), // size of entire drawing area
+		CanvasOffset: fyne.NewPos(0, 0),
+		PxRows:       10,
+		PxCols:       10,
+		PxSize:       30, // size of each pixel square
+	}
+	pixlCanvas := pxcanvas.NewPxCanvas(&state, pixlCanvasConfig)
+
 	appInit := ui.AppInit{
+		PixlCanvas: pixlCanvas,
 		PixlWindow: pixlWindow,
 		State:      &state,
 		Swatches:   make([]*swatch.Swatch, 0, 64),
--- ui/types.go
+++ ui/types.go
@@ -2,12 +2,14 @@ package ui
 
 import (
 	"pixel-art-editor-golang/apptype"
+	"pixel-art-editor-golang/pxcanvas"
 	"pixel-art-editor-golang/swatch"
 
 	"fyne.io/fyne/v2"
 )
 
 type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
 	PixlWindow fyne.Window
 	State      *apptype.State
 	Swatches   []*swatch.Swatch

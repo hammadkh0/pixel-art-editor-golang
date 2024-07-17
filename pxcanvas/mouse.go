1c74ca837c998ec8a0b0eb2567691147c3114f29
hammadkh0 <hammad5718@gmail.com>
2024-07-17 12:52:26 +0500

    feat: painting pixels + dragging mouse functionality

--- pxcanvas/mouse.go
+++ pxcanvas/mouse.go
@@ -1,6 +1,8 @@
 package pxcanvas
 
 import (
+	"pixel-art-editor-golang/pxcanvas/brush"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/driver/desktop"
 )
@@ -12,8 +14,22 @@ func (pxCanvas *PxCanvas) Scrolled(ev *fyne.ScrollEvent) {
 
 func (pxCanvas *PxCanvas) MouseIn(ev *desktop.MouseEvent) {}
 func (pxCanvas *PxCanvas) MouseOut()                      {}
+
 func (pxCanvas *PxCanvas) MouseMoved(ev *desktop.MouseEvent) {
+	if x, y := pxCanvas.MouseToCanvasXY(ev); x != nil && y != nil {
+		brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
+		cursor := brush.Cursor(pxCanvas.PxCanvasConfig, pxCanvas.appState.BrushType, ev, *x, *y)
+		pxCanvas.renderer.SetCursor(cursor)
+	} else {
+		pxCanvas.renderer.SetCursor(make([]fyne.CanvasObject, 0)) //hide the cursor when mouse is moved outside the canvas
+	}
 	pxCanvas.TryPan(pxCanvas.mouseState.previousCoord, ev)
 	pxCanvas.Refresh()
 	pxCanvas.mouseState.previousCoord = &ev.PointEvent // update the coordinate after the panning is done so this value can be used in next panning
 }
+
+// Mouseable Interface implementation
+func (pxCanvas *PxCanvas) MouseDown(ev *desktop.MouseEvent) {
+	brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
+}
+func (pxCanvas *PxCanvas) MouseUp(ev *desktop.MouseEvent) {}
